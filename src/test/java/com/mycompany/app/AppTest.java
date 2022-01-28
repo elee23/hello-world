ca458959c8e8eab76f07391f538c58413d882a26
Elaine Lee <elaine@icatalystinc.com>
2022-01-28 17:58:28 -0500

    Update for Selenium

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -4,17 +4,30 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
+public class AppTest {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
+    public void shouldAnswerWithTrue() {
         assertTrue( true );
+    }
+
+    /**
+     * Open browser test
+     */
+    @Test
+    public void edgeSession() {
+      EdgeOptions options = new EdgeOptions();
+      WebDriver driver = new EdgeDriver(options);
+
+      driver.quit();
     }
 }
